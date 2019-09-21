f67f50ad83996e242e37ae859d25fa0930a44745
Jigar Kapadia <jbk93i@gmail.com>
2019-09-21 16:37:59 -0400

    removing un-needed code

--- src/twoSum1/TwoSum.java
+++ src/twoSum1/TwoSum.java
@@ -1,38 +1,27 @@
 package twoSum1;
 
-import java.util.HashSet;
-import java.util.Hashtable;
+import java.util.HashMap;
+import java.util.Map;
 
 public class TwoSum {
 
 	public static int[] returnPair(int[] nums, int target) {
-		int[] resultPair = new int[2];
-		HashSet<Integer> numTable = new HashSet<Integer>();
-		
-		if(nums.length<2){
-			System.out.println( "Need at least two numbers!");
-			return(null);
-		}	
-		
-		for(int count = 0; count< nums.length; count++) 
-			numTable.add(nums[count]);
-		
-		for(int count = 0; count< nums.length; count++){
-			int currentNum = nums[count];
-			int requiredNum = target - currentNum;
-			
-			if(numTable.contains(requiredNum)){
-				resultPair[0] = currentNum;
-				resultPair[1] = requiredNum;
-				return(resultPair);
-			}
-		}
-		return(resultPair);
+		Map<Integer, Integer> map = new HashMap<>();
+	    for (int count = 0; count < nums.length; count++) {
+	        map.put(nums[count], count);
+	    }
+	    for (int count = 0; count < nums.length; count++) {
+	        int requiredNum = target - nums[count];
+	        if (map.containsKey(requiredNum) && map.get(requiredNum) != count) {
+	            return new int[] { count, map.get(requiredNum) };
+	        }
+	    }
+	    throw new IllegalArgumentException("Error!");
     }
 	
 	public static void main(String[] args) {
-		int[] nums = new int[]{3,4,8,9,0,1};
-		int[] resultPair = returnPair(nums, 7);
+		int[] nums = new int[]{0,3,0,3};
+		int[] resultPair = returnPair(nums, 6);
 		
 		if(resultPair != null)
 			for( int number: resultPair)
